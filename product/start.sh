fcd3b612e8ca28a7fc7d59c0cd6d9660d402f68c
Do LE <do.le@metanet.co.kr>
2020-11-22 12:54:07 +0700

    Modify start.sh 'add sleeping 10 seconds for waiting container DB is reade before starting Tomcat server'

--- product/start.sh
+++ product/start.sh
@@ -1,4 +1,6 @@
 docker network create product-mysql
 docker container run --name mysqldb --network product-mysql -e MYSQL_ROOT_PASSWORD=123@Toor -e MYSQL_DATABASE=product -d mysql:8
 docker image build -t product .
+echo "Waiting for 10 seconds for MYSQL Database is ready..."
+sleep 10
 docker container run --network product-mysql --name product-container -p 8080:8080 -d product
